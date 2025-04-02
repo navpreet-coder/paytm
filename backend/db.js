3b53df62f9121cb04729af6afd35b4f22986a90d
navpreet-coder <navuk95@gmail.com>
2025-04-02 08:44:05 +0530

    created routing part 1

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/paytm')
+//basic solution
+// const userSchema = mongoose.Schema({
+//     username:String,
+//     password:String,
+//     firstName:String,
+//     lastName:String
+// })
 
+//elegant solution
 const userSchema = mongoose.Schema({
-    username:String,
-    password:String,
-    firstName:String,
-    lastName:String
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        minLength:3,
+        maxLength:30
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    }
 })
 
 const User = mongoose.model("user",userSchema)
